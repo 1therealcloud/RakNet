1cf9dbb227d1377c94c3b097352b646a4dfdc46b
1therealcloud <1therealcloud@gmail.com>
2026-09-15 19:48:32 +0300

    InternalPacketPool.cpp - move index into loo

--- source/InternalPacketPool.cpp
+++ source/InternalPacketPool.cpp
@@ -21,8 +21,7 @@ InternalPacketPool::InternalPacketPool()
 {	
 	// Speed things up by not reallocating at runtime when a mutex is locked.
 	pool.ClearAndForceAllocation( 64 );
-	unsigned i;
-	for (i=0; i < 64; i++)
+	for (unsigned i=0; i < 64; i++)
 		pool.Push(new InternalPacket);
 }
 
